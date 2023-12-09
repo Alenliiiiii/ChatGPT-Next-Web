107b5f0a9f10e546e4779b699ad4ce6805c211b1
Alenliiiiii <153336208+Alenliiiiii@users.noreply.github.com>
2023-12-09 11:49:57 +0800

    Update settings.tsx

--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -225,44 +225,6 @@ function UserPromptModal(props: { onClose?: () => void }) {
   );
 }
 
-function DangerItems() {
-  const chatStore = useChatStore();
-  const appConfig = useAppConfig();
-
-  return (
-    <List>
-      <ListItem
-        title={Locale.Settings.Danger.Reset.Title}
-        subTitle={Locale.Settings.Danger.Reset.SubTitle}
-      >
-        <IconButton
-          text={Locale.Settings.Danger.Reset.Action}
-          onClick={async () => {
-            if (await showConfirm(Locale.Settings.Danger.Reset.Confirm)) {
-              appConfig.reset();
-            }
-          }}
-          type="danger"
-        />
-      </ListItem>
-      <ListItem
-        title={Locale.Settings.Danger.Clear.Title}
-        subTitle={Locale.Settings.Danger.Clear.SubTitle}
-      >
-        <IconButton
-          text={Locale.Settings.Danger.Clear.Action}
-          onClick={async () => {
-            if (await showConfirm(Locale.Settings.Danger.Clear.Confirm)) {
-              chatStore.clearAllData();
-            }
-          }}
-          type="danger"
-        />
-      </ListItem>
-    </List>
-  );
-}
-
 function CheckButton() {
   const syncStore = useSyncStore();
 
